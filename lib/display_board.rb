cbd4a59a394cdc58289d0255b30814a4bbe9a0bf
EZZELDEN ABDELMGED <ezzeldensobhy@yahoo.com>
2017-09-16 10:47:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "Tic Tac Toe board"
-  3.times{puts  "   " + "|" +"   "  +"|"}
+  puts  "   " + "|" +"   "  +"|"
 
 end
 
