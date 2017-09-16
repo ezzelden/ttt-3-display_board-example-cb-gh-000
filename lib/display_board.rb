5ccf7691773decdb9b3ca4b59443ad31ce8a1533
EZZELDEN ABDELMGED <ezzeldensobhy@yahoo.com>
2017-09-16 10:52:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,10 +4,6 @@ def display_board
 puts "Tic Tac Toe board"
   puts  "   " + "|" +"   "  +"|"
   puts  "-----------"
-  puts  "   " + "|" +"   "  +"|"
-  puts  "-----------"
-  puts  "   " + "|" +"   "  +"|"
-
 
 end
 
